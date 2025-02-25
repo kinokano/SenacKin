26b35191896b124f0bc2dcddf529f18144b7278c
kinokano <kin2014@hotmail.com>
2025-02-24 21:10:01 -0400

    3

--- Django/backend/api/urls.py
+++ Django/backend/api/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-from .views import (listarFuncionarios, cadastrarFuncionario, obterFuncionario)
+from django.urls import path, include
+# from .views import (listarFuncionarios, cadastrarFuncionario, obterFuncionario)
+from rest_framework.routers import DefaultRouter
+from api.views import FuncionarioViewSet
+
+router = DefaultRouter()
+router.register('funcionarios', FuncionarioViewSet)
 
 urlpatterns = [
-    path('funcionarios/', listarFuncionarios, name='listarFuncionarios'),
-    path('funcionarios/cadastrar', cadastrarFuncionario, name='cadastrarFuncionario'),
-    path('funcionarios/<int:id>', obterFuncionario, name='obterFuncionario'),
+    path('', include(router.urls))
+
+    # path('funcionarios/', listarFuncionarios, name='listarFuncionarios'),
+    # path('funcionarios/cadastrar', cadastrarFuncionario, name='cadastrarFuncionario'),
+    # path('funcionarios/<int:id>', obterFuncionario, name='obterFuncionario'),
+
 
 ]
